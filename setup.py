e8a0631a4ec084fd6ae8fa5b19773baf1e0e4c73
hwangeug <hwang.eug@gmail.com>
2018-11-17 21:37:59 -0800

    long_description in setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,14 @@ from distutils.core import setup
 setup(
     name = 'lutris-bulk-adder',
     packages = ['lutris_bulk_adder'],
-    version = '0.1.4',
+    version = '0.1.5',
     license='MIT',
     description = 'Python script to bulk import a directory of ROM files into Lutris',
+    long_description = """
+This is a simple Python script to import a directory of ROM files into Lutris.
+The directory must contain only games from one platform.  I have only tested this
+on my own computer for a limited number of platforms.
+""",
     author = 'Eugene Hwang',
     author_email = 'hwang.eug@gmail.com',
     url = 'https://github.com/hwangeug/lutris-bulk-adder',
